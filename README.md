7c9597c59a32c64c35ec77ef21c0ccc35a913767
Yusuf Enes <yusufenes.doganay@gmail.com>
2019-10-28 16:27:05 +0300

    Added the functionality to perform the operations by operation priority. Removed unnecessary codes. Added examples to README file.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # basic-calculator
 A basic calculator script a made with Python 3.
 
-After you run the script, all you need to do is writing the expression you would like with out without spaces and the script gives you the result.
+After you run the script, all you need to do is to write the expression you would like and the script gives you the result.
+
+<pre>Example inputs: -5 + (10 + 5) / 2 * 3   //Output: 17.5
+                -5+(10+5)/2*3           //Output: 17.5
+                -5 +(10+ 5) /2 *3       //Output: 17.5 </pre>
--- calculator.py
+++ calculator.py
@@ -1,29 +1,5 @@
-import operator
+operation = input("Enter the expression: ")
 
-operatorlookup = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-    '/': operator.truediv
-}
-
-operation = input("İşlemi giriniz: ")
-
-result = 0
-
-elements = []
-
-for z in operation:
-    if z is not " ":
-        elements.append(z)
-
-for x in range(0, len(elements), 2):
-    y = int(elements[x])
-
-    if x == 0:
-        result += y
-    else:
-        op = operatorlookup.get(elements[x-1])
-        result = op(result, y)
+result = eval(operation)
 
 print("Result: ", result)
